e3b2eeaed8663a43c9352dcc6205c16f3278cce2
Misha <hryvinskyim@gmail.com>
2025-04-02 20:58:28 +0300

    added mobile-menu script

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 import "../scss/style.scss";
+import { menuOpen, menuClose } from "./mobile-menu";
+
+const menuCloseBtn = document.querySelector('.header__button-close');
+const menuOpenBtn = document.querySelector('.js-menu-open');
+
+menuOpenBtn.addEventListener('click',menuOpen);
+menuCloseBtn.addEventListener('click', menuClose);
---
+++ src/js/mobile-menu.js
@@ -0,0 +1,17 @@
+const headerNav = document.querySelector('.header__nav');
+
+function menuOpen() {
+    headerNav.classList.add('open');
+    document.body.style.overflow = 'hidden';
+}
+   
+  
+function menuClose(){
+    headerNav.classList.remove('open');
+    document.body.style.overflow = '';
+}
+
+export { 
+    menuClose, 
+    menuOpen
+}
