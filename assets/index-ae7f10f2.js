b16213c2445cd905b54a5ac87103f417b898e07c
MishaHryvinskyi <116170668+MishaHryvinskyi@users.noreply.github.com>
2025-04-02 18:54:18 +0000

    Deploying to gh-pages from @ MishaHryvinskyi/vite-template@aa75667b1161cfb8babaf6cc15e4c6633c3d295a 🚀

--- assets/index-ae7f10f2.js
+++ assets/index-ae7f10f2.js
@@ -49,7 +49,25 @@ function menuClose() {
   headerNav.classList.remove("open");
   document.body.style.overflow = "";
 }
+const backdrop$1 = document.querySelector(".backdrop");
+function openModal() {
+  backdrop$1.classList.remove("is-hidden");
+}
+function closeModal() {
+  backdrop$1.classList.add("is-hidden");
+}
+function onBackdropClick(e) {
+  if (e.target === e.currentTarget) {
+    closeModal();
+  }
+}
 const menuCloseBtn = document.querySelector(".header__button-close");
 const menuOpenBtn = document.querySelector(".js-menu-open");
+const openModalBtn = document.querySelector(".js-hero-button");
+const closeModalBtn = document.querySelector(".js-modal-close");
+const backdrop = document.querySelector(".backdrop");
 menuOpenBtn.addEventListener("click", menuOpen);
 menuCloseBtn.addEventListener("click", menuClose);
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+backdrop.addEventListener("click", onBackdropClick);
