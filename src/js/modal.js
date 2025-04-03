d9bb9ab9375489c3c4f5dfe7040b2a6d8368eb8a
Misha <hryvinskyim@gmail.com>
2025-04-03 19:04:03 +0300

    added function onEscapeClick styled and markup svg inner input

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,21 +1,33 @@
 const backdrop = document.querySelector('.backdrop');
 
 function openModal() {
+    window.addEventListener("keydown", onEscapeClick);
     backdrop.classList.remove('is-hidden')
-}
+};
 
 function closeModal() {
+    window.removeEventListener("keydown", onEscapeClick);
     backdrop.classList.add('is-hidden')
-}
+};
 
 function onBackdropClick(e) {
     if(e.target === e.currentTarget) {
         closeModal();
     }
-}
+};
+
+function onEscapeClick(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = event.code === ESC_KEY_CODE;
+    
+    if(isEscKey) {
+        closeModal();
+    }
+};
 
 export {
     openModal,
     closeModal,
-    onBackdropClick
-}
+    onBackdropClick,
+    onEscapeClick
+};
