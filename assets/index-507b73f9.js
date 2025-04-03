65d3a4d50b65371b5f93f11005e8fda6a4bdc36b
MishaHryvinskyi <116170668+MishaHryvinskyi@users.noreply.github.com>
2025-04-03 16:04:19 +0000

    Deploying to gh-pages from @ MishaHryvinskyi/vite-template@d9bb9ab9375489c3c4f5dfe7040b2a6d8368eb8a 🚀

--- assets/index-507b73f9.js
+++ assets/index-507b73f9.js
@@ -51,13 +51,22 @@ function menuClose() {
 }
 const backdrop$1 = document.querySelector(".backdrop");
 function openModal() {
+  window.addEventListener("keydown", onEscapeClick);
   backdrop$1.classList.remove("is-hidden");
 }
 function closeModal() {
+  window.removeEventListener("keydown", onEscapeClick);
   backdrop$1.classList.add("is-hidden");
 }
 function onBackdropClick(e) {
   if (e.target === e.currentTarget) {
+    closeModal();
+  }
+}
+function onEscapeClick(event) {
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = event.code === ESC_KEY_CODE;
+  if (isEscKey) {
     closeModal();
   }
 }
