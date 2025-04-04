8dc89eb58ccee3e77ec36ae63825f74440b4b269
MishaHryvinskyi <116170668+MishaHryvinskyi@users.noreply.github.com>
2025-04-04 13:10:06 +0000

    Deploying to gh-pages from @ MishaHryvinskyi/web-studio@548c98681e437fa963f986627246ece98e7c3bc7 🚀

--- assets/index-74ba62f7.js
+++ assets/index-74ba62f7.js
@@ -41,6 +41,7 @@
 })();
 const style = "";
 const headerNav = document.querySelector(".header__nav");
+const anchorLinks = document.querySelectorAll(".header__nav-link");
 function menuOpen() {
   headerNav.classList.add("open");
   document.body.style.overflow = "hidden";
@@ -49,6 +50,13 @@ function menuClose() {
   headerNav.classList.remove("open");
   document.body.style.overflow = "";
 }
+anchorLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (headerNav.classList.contains("open")) {
+      menuClose();
+    }
+  });
+});
 const backdrop$1 = document.querySelector(".backdrop");
 function openModal() {
   window.addEventListener("keydown", onEscapeClick);
