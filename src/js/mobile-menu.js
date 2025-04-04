f26a27f682313dd0d20b612e20da01fa8fdcd6fb
Misha <hryvinskyim@gmail.com>
2025-04-04 14:02:49 +0300

    Closing the mobile menu when clicking on some links

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,4 +1,5 @@
 const headerNav = document.querySelector('.header__nav');
+const anchorLinks = document.querySelectorAll('.header__nav-link');
 
 function menuOpen() {
     headerNav.classList.add('open');
@@ -9,6 +10,14 @@ function menuClose(){
     headerNav.classList.remove('open');
     document.body.style.overflow = '';
 }
+
+anchorLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (headerNav.classList.contains('open')) {
+            menuClose();
+        }
+    });
+});
 
 export { 
     menuClose, 
